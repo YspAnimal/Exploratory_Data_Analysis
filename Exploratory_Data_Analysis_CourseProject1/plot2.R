c095bfe77037d52e30800c652a6fa24efb987a06
YspAnimal <Animal1986@mail.ru>
2015-10-19 09:50:50 +0400

    Reconf

--- Exploratory_Data_Analysis_CourseProject1/plot2.R
+++ Exploratory_Data_Analysis_CourseProject1/plot2.R
@@ -1,5 +1,4 @@
-#set my working directory
-setwd("C:/Users/soloveynv/Documents/R Scripts/Coursera/Exploratory_Data_Analysis")
+#Work in default working directory
 #Download file, unzip it if it does not exist
 URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 destFile <- "power_consumption.zip"
@@ -7,7 +6,7 @@ if (!file.exists(destFile)){
   download.file(URL, destfile = destFile, mode='wb')
   unzip(destFile)
 }
-#Try to read subset of data using SQL methods 
+#Try to read subset of data using SQL methods(just for practice) 
 library(RSQLite)
 # Create/Connect to a database
 con <- dbConnect(RSQLite::SQLite(), dbname = ":memory:")
@@ -20,4 +19,28 @@ dbWriteTable(con, name="household_power_consumption", value="household_power_con
 PowerDataFeb <- dbGetQuery(con, "SELECT * FROM household_power_consumption WHERE (Date = '1/2/2007' or Date = '2/2/2007')")
 #Close connection
 dbDisconnect(con)
+
+#Convert the Date and Time variables to Date/Time classes 
+PowerDataFeb$Date <- as.Date(PowerDataFeb$Date, format = "%d/%m/%Y")
+PowerDataFeb$timetemp <- paste(PowerDataFeb$Date, PowerDataFeb$Time)
+PowerDataFeb$Time <- strptime(PowerDataFeb$timetemp, format = "%Y-%m-%d %H:%M:%S")
+
+#Open graphic device PNG and Draw figure LinesPlot
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
+plot(PowerDataFeb$Time, PowerDataFeb$Global_active_power, type = "l", col = "black", ylab = "Globa Active Power (kilowatts)", xlab = "")
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
